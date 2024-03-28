eedadff85ad8ce495507a737dcf2bb8217f72e31
MOHAMMED <120588577+MOHAMMED2207@users.noreply.github.com>
2024-03-28 03:48:12 +0200

    Update Header.tsx

--- app/Header/Header.tsx
+++ app/Header/Header.tsx
@@ -6,18 +6,18 @@ import Navbar from "./Navbar";
 
 import axios from "axios";
 const Header = ({ placeholder }: { placeholder?: string }) => {
-
-
-  
   return (
-    <header className=" relative top-0 z-50 bg-white shadow-md p-5">
-      <div className="container grid md:grid-cols-3 lg:grid-cols-3 grid-cols-1 w-full place-content-center">
-        <Link href="/" className="relative flex items-center my-auto h-10">
+    <header className=" relative top-0 z-50 bg-white shadow-md p-5  ">
+      <div className="container grid md:grid-cols-3 lg:grid-cols-3   w-full ">
+        <Link
+          href="/"
+          className="relative flex items-center my-auto h-10 w-full"
+        >
           <Image
             src="https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg"
             alt="logo-img"
             fill
-            className="object-contain object-left "
+            className="object-contain object-center lg:object-left md:object-left"
           />
         </Link>
         <Searchbar placeholder={placeholder} />
