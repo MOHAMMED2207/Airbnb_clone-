1b525bd4ac9d05315b8d054e553e11ba7ea02d2a
MOHAMMED <120588577+MOHAMMED2207@users.noreply.github.com>
2024-03-28 03:48:29 +0200

    Update Navbar.tsx

--- app/Header/Navbar.tsx
+++ app/Header/Navbar.tsx
@@ -2,7 +2,7 @@ import { GlobeAltIcon, MenuIcon, UserCircleIcon } from '@heroicons/react/solid';
 
 const Navbar = () => {
   return (
-    <div className='flex space-x-4 items-center justify-end text-gray-500'>
+    <div className='flex space-x-4 items-center justify-end hidden lg:flex md:flex text-gray-500'>
       <p className='hidden md:inline cursor-pointer'>Become a host</p>
       <GlobeAltIcon className='h-6 cursor-pointer' />
       <div className='flex space-x-2 border-2 p-2 rounded-full'>
